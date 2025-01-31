b7c363f259123d13b85155d70d33b9c46dd29caa
User <user@Users-iMac.local>
2025-01-31 17:22:21 +1300

    port forward public IP Mac setup

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -6,7 +6,8 @@ export const StoreContext = createContext(null);
 export const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({});
-    const url = "http://localhost:4000";
+    //const url = "http://localhost:4000";
+    const url = "http://203.94.61.20:4000"
 
     const [token, setToken] = useState("");
 
