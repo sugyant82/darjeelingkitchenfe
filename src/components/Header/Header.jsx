c8aad86c51668ab372bf7d33d073de7b8e84e61e
User <user@Users-iMac.local>
2025-04-04 14:03:16 +1300

    hero p text mobile unhide and fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -44,7 +44,7 @@ const Header = () => {
       <div className="header-contents">
         <h2>Authentic Flavors in Auckland</h2>
         <p>
-          At Darjeeling Kitchen, we bring authentic Nepalese and Indian flavors to Auckland. From classic momos to
+          At Darjeeling Momo NZ, we bring authentic Nepalese and Indian flavors to Auckland. From classic momos to
           aromatic curries, every dish is crafted with traditional recipes and fresh ingredients, offering you a taste
           of home in every bite.
         </p>
