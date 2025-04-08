0b81ca8079a8f48457a7b2b094312d35ea0714de
User <user@Users-iMac.local>
2025-04-09 00:59:17 +1200

    google IAM changes hardcode vite env

--- src/fireapp.js
+++ src/fireapp.js
@@ -1,12 +1,12 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.FIREBASE_API_KEY,
-  authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.FIREBASE_MSG_SENDER_ID,
-  appId: import.meta.env.FIREBASE_APP_ID,
+  apiKey: "AIzaSyATiO5TNhQC0_GLNkKJiXVALEyxfd360G0",
+  authDomain: "darjeelingmomonz-67dce.firebaseapp.com",
+  projectId: "darjeelingmomonz-67dce",
+  storageBucket: "darjeelingmomonz-67dce.appspot.com",
+  messagingSenderId: "46977250515",
+  appId: "1:46977250515:web:6306122f183a8a4d0d1a2e",
 };
 
 const fireapp = initializeApp(firebaseConfig);
