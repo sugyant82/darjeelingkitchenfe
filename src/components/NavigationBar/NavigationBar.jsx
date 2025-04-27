8dbec98d1b56592ece81edc6bb9bed4fb6359d6e
User <user@Users-iMac.local>
2025-04-27 17:04:31 +1200

    cors policy update

--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -28,7 +28,7 @@ const NavigationBar = ({setShowLogin}) => {
       <Link to='/'><img src={assets.logo} alt="" className="logo" /></Link>
       <ul className="navbar-menu">
         <Link to='/' onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>Home</Link>
-        <a href='#explore-menu' onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>Menu</a>
+        <a href='#explore-menu' onClick={()=>{setMenu("menu");window.scrollTo({ top: 50, behavior: 'smooth' }); }} className={menu==="menu"?"active":""}>Menu</a>
         <a href='#app-download' onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>Mobile App</a>
         <a href='#footer' onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>Contact Us</a>
       </ul>
